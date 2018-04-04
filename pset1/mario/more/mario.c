859379635552f7de084c8b3ae7fa7664f0c80b18
Sean Cahill <seancahill25@gmail.com>
2018-04-04 20:47:41 +0000

    did mario problems

---
+++ pset1/mario/more/mario.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int n;
+
+    do
+    {
+        n = get_int("Height: ");
+    }
+    while (n < 0 || n > 23);
+
+    for (int i = 0; i < n; i++)
+    {
+//s is for spaces
+        for (int s = 0; s < n - i - 1; s++)
+        {
+            printf(" ");
+        }
+//h is for hashes
+        for (int h = 0; h < i + 1; h++)
+        {
+            printf("#");
+        }
+        printf("  ");
+        for (int h = 0; h < i + 1;  h++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
+
