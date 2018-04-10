6f78671afc3f619be4fbe3ede839420df2001c9d
Sean Cahill <seancahill25@gmail.com>
2018-04-10 14:48:46 +0000

    fixed and completed caesar

--- pset2/caesar/caesar.c
+++ pset2/caesar/caesar.c
@@ -8,25 +8,29 @@ int main(int argc, string argv[])
 {
     int key = 0;
     string s;
-    while(argc == 2)
+    if (argc == 2)
     {
         key = atoi(argv[1]);
         s = get_string("plaintext: ");
-        break;
+    }
+    else
+    {
+        printf("Try again\n");
+        return 1;
     }
     printf("ciphertext: ");
-    for(int i = 0, n = strlen(s); i < n; i++)
+    for (int i = 0, n = strlen(s); i < n; i++)
     {
-        if(isalpha(s[i]))
+        if (isalpha(s[i]))
         {
-            if(isupper(s[i]))
+            if (isupper(s[i]))
             {
-                char t = (((s[i] - 65) + key) %26) + 65;
+                char t = (((s[i] - 65) + key) % 26) + 65;
                 printf("%c", t);
             }
             else
             {
-                char t = (((s[i] - 97) + key) %26) + 97;
+                char t = (((s[i] - 97) + key) % 26) + 97;
                 printf("%c", t);
             }
         }
