1df4ad5b87ffe52091a38bc6325b024ea3991790
Sean Cahill <seancahill25@gmail.com>
2018-04-14 22:40:37 +0000

    downloaded necessary files and completed is_rest function

---
+++ pset3/music/helpers.c
@@ -0,0 +1,30 @@
+// Helper functions for music
+
+#include <cs50.h>
+#include <stdlib.h>
+#include "helpers.h"
+
+// Converts a fraction formatted as X/Y to eighths
+int duration(string fraction)
+{
+
+}
+
+// Calculates frequency (in Hz) of a note
+int frequency(string note)
+{
+    // TODO
+}
+
+// Determines whether a string represents a rest
+bool is_rest(string s)
+{
+    if (s = "")
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
