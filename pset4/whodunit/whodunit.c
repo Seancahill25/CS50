562d0efc17a55707d6c04e62734de169839fe4f8
Sean Cahill <seancahill25@gmail.com>
2018-04-22 22:02:09 +0000

    set up whodunit.c for error checking

---
+++ pset4/whodunit/whodunit.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(int argc, string argv[])
+{
+    if (argc != 3)
+    {
+        printf("Usage: ./whodunit infile outfile");
+        return 1;
+    }
+    else
+    {
+
+    }
+}
