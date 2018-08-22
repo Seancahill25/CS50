8189b034904d0fd7f0ea75fd4ed6abf9e2e34250
Sean Cahill <seancahill25@gmail.com>
2018-08-22 03:54:22 +0000

    finished pset6

---
+++ pset6/similarities/helpers.py
@@ -0,0 +1,28 @@
+from nltk.tokenize import sent_tokenize
+def lines(a, b):
+    linesA = set(a.split("\n"))
+    linesB = set(b.split("\n"))
+
+    return linesA & linesB
+
+def sentences(a, b):
+    sentencesA = set(sent_tokenize(a))
+    sentencesB = set(sent_tokenize(b))
+
+    return sentencesA & sentencesB
+
+def substringLength(str, n):
+    substrings = []
+
+    for i in range(len(str) - n + 1):
+        substrings.append(str[i:i + n])
+
+    return substrings
+
+def substrings(a, b, n):
+    substringsA = set(substringLength(a, n))
+    substringsB = set(substringLength(b, n))
+
+    return substringsA & substringsB
+
+
